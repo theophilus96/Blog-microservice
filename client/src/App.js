f9ea9bc7bb981495b0c59ff39682963edead5a6c
theo <theophilus96@gmail.com>
2020-12-23 15:58:26 +0800

    microservice part 1

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
